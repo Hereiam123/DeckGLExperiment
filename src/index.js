220f09800d4f562940ba91e1d00038b9b2302a55
hereiam123 <bdemaio1@gmail.com>
2019-12-08 19:44:54 -0500

    Adding in tooltip if user clicks on data point

--- src/index.js
+++ src/index.js
@@ -12,7 +12,25 @@ const scatterplot = () =>
     radiusMinPixels: 2,
     radiusMaxPixels: 5,
     getPosition: d => [d.Long, d.Lat],
-    getFillColor: d => (d.Shooting > 0 ? [200, 0, 40, 150] : [255, 140, 0, 100])
+    getFillColor: d =>
+      d.Shooting > 0 ? [200, 0, 40, 150] : [255, 140, 0, 100],
+    pickable: true,
+    onClick: ({ object, x, y }) => {
+      const el = document.getElementById("tooltip");
+      if (object) {
+        const { OCCURRED_ON_DATE, OFFENSE_DESCRIPTION } = object;
+        el.innerHTML = `<h1>${OCCURRED_ON_DATE} Type: ${OFFENSE_DESCRIPTION}</h1>`;
+        el.style.position = "fixed";
+        el.style.display = "block";
+        el.style.backgroundColor = "white";
+        el.style.opacity = 0.9;
+        el.style.left = x + "px";
+        el.style.top = y + "px";
+      } else {
+        el.style.opacity = 0.0;
+        el.style.display = "none";
+      }
+    }
   });
 
 window.initMap = () => {
